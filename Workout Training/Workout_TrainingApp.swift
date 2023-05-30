48cb74b36ccb63ec3f51ccde223c5f7c97be73f8
tommasocastellanza <tcastell@students.cs.ubc.ca>
2023-05-29 21:59:38 -0700

    Initial Commit

---
+++ Workout Training/Workout_TrainingApp.swift
@@ -0,0 +1,17 @@
+//
+//  Workout_TrainingApp.swift
+//  Workout Training
+//
+//  Created by Tommaso Castellanza on 2023-05-29.
+//
+
+import SwiftUI
+
+@main
+struct Workout_TrainingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
